4453f9c40bf1528cabf41b79fb53c04d0757e57a
evanyudia-ship-it <evanyudia@gmail.com>
2026-06-27 19:09:06 +0800

    Update 2026_06_27_095902_create_faculties_table.php

--- database/migrations/2026_06_27_095902_create_faculties_table.php
+++ database/migrations/2026_06_27_095902_create_faculties_table.php
@@ -6,20 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('faculties', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name');
+            $table->string('code')->unique();
+            $table->text('description')->nullable();
+
+            $table->string('status')->default('active');
+            // active, inactive
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('faculties');
