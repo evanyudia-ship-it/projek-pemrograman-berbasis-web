b51b82dfcb8fabd4706a2ee13e7f534b718dde16
evanyudia-ship-it <evanyudia@gmail.com>
2026-06-27 19:09:29 +0800

    Update 2026_06_27_095908_create_admin_faculties_table.php

--- database/migrations/2026_06_27_095908_create_admin_faculties_table.php
+++ database/migrations/2026_06_27_095908_create_admin_faculties_table.php
@@ -6,20 +6,31 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('admin_faculties', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->cascadeOnDelete();
+
+            $table->foreignId('faculty_id')
+                ->constrained('faculties')
+                ->cascadeOnDelete();
+
+            $table->string('position')->nullable();
+            // contoh: validator, admin fakultas, kepala bagian
+
+            $table->string('status')->default('active');
+            // active, inactive
+
             $table->timestamps();
+
+            $table->unique(['user_id', 'faculty_id']);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('admin_faculties');
